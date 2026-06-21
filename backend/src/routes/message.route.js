69341cbe45a769b1a2c47356c3185cebcfa6a524
Aakashth2025 <aakash1505at@gmail.com>
2026-06-21 05:57:50 +0530

    design of signup page

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express"
-import { protectRoute } from "../middlewares/auth.middleware";
-import { getMessages, getUserForSidebar, sendMessages } from "../controllers/message.controller";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { getMessages, getUserForSidebar, sendMessages } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users", protectRoute, getUserForSidebar);
