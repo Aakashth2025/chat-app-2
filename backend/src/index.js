827c7282609d8e99e9e3e45d5a84a0c916cd80ec
Aakashth2025 <aakash1505at@gmail.com>
2026-07-08 23:24:12 +0530

    prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,13 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 //const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({
@@ -24,6 +26,13 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+    });
+}
 
 server.listen(5001, ()=>{
     console.log("server is running on port : " + PORT);
