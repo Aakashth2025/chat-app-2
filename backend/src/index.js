28ca96f322ad25ddaea38e0db7c1b0f530bf5dba
Aakashth2025 <aakash1505at@gmail.com>
2026-07-08 23:52:39 +0530

    issue2

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,10 +11,10 @@ import path from "path";
 //const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json({limit: '10mb'}));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({
     extended: true
 }));
@@ -27,14 +27,14 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 
-server.listen(5001, ()=>{
+server.listen(PORT, () => {
     console.log("server is running on port : " + PORT);
     connectDB();
 });
